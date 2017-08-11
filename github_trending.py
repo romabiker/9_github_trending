f05b0b1695468807594759ca48737a2d0e88f57a
Valery Ramusik <valer2677.@gmailcom>
2017-08-11 03:35:15 +0300

    Refactor github_trending.py
    -add argparser
    -change some names vars to more appropriate

--- github_trending.py
+++ github_trending.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import json
 import requests
@@ -11,39 +12,36 @@ def get_top_urls_with_issues(repositories_json):
             ]
 
 
-def output_urls_with_issues_to_console(urls_with_issues):
+def output_urls_with_issues_to_console(urls_with_issues, rep_count, days):
     print('\n Top {} github repositories created for the last {} days:\n'
-          .format(days, repos_quantity))
+          .format(rep_count, days))
     print('№   {:<70} | {:<10}'.format('URL', 'Open issues'))
     for count, (url, issue) in enumerate(urls_with_issues, 1):
         print('{:<3} {:<70} | {:<10}'.format(count, url, issue))
     print()
 
 
-def prompt_days_repos_quantity():
-    try:
-        return (
-                int(input('Enter days:  ')),
-                int(input('Enter quantity:  '))
-                )
-    except ValueError as e:
-        print('You have enter digits')
-        print('Example: Enter days:  7')
-        print('Example: Enter quantity:  20')
-        sys.exit()
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--days', default=7, type=int,
+                        help='number of the days')
+    parser.add_argument('-c', '--count', default=30, type=int,
+                        help='quantity of the top repositories')
+    return parser
 
 
-def request_new_github_repos_api(days, quantity):
+def request_new_github_repos_api(days, rep_count):
     github_repositories = 'https://api.github.com/search/repositories'
     payload = {
-                'q': {'created': stringify_passed_days(days=days),
-                      'sort': 'stars',
-                      'order': 'desc'}
+              'q': {'created': stringify_passed_days(days=days),
+                    'sort': 'stars',
+                    'order': 'desc'},
+              'per_page': rep_count,
               }
     response = requests.get(github_repositories, params=payload)
     if not response.ok:
         response.raise_for_status()
-    return json.loads(response.text)['items'][:quantity]
+    return json.loads(response.text)['items']
 
 
 def stringify_passed_days(days):
@@ -52,7 +50,9 @@ def stringify_passed_days(days):
 
 
 if __name__ == '__main__':
-    days, repos_quantity = prompt_days_repos_quantity()
-    new_github_repos_json = request_new_github_repos_api(days, repos_quantity)
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    days, rep_count = namespace.days, namespace.count
+    new_github_repos_json = request_new_github_repos_api(days, rep_count)
     top_urls_with_issues = get_top_urls_with_issues(new_github_repos_json)
-    output_urls_with_issues_to_console(top_urls_with_issues)
+    output_urls_with_issues_to_console(top_urls_with_issues, rep_count, days)
